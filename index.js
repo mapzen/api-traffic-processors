6dff647ae8ee4b3b156e09cdcd2ee919f8116bc5
Evan Griffiths <griffithse@gmail.com>
2016-04-07 11:12:26 -0400

    handle qpd/qps

--- index.js
+++ index.js
@@ -17,16 +17,19 @@ module.exports = function() {
 //   timing
 //   is_keyless
 //   parsed_url
+//   error
 function formatHit(hit) {
-  // var payload = {
-  //   timestamp: new Date().getTime(),
-  //   api: hit.api_name,
-  //   key: hit.key_name,
-  //   status: hit.status_code,
-  //   query: hit.parsed_url ? hit.parsed_url.search : null,
-  //   pathname: hit.parsed_url ? hit.parsed_url.pathname : null,
-  // };
-  return Object.keys(hit).map(function(key) {return encode(payload[key]);}).join(',');
+  var payload = {
+    timestamp: new Date().getTime(),
+    api: hit.api_name,
+    key: hit.key_name,
+    status: hit.error
+            ? hit.error.name
+            : hit.status_code,
+    query: hit.parsed_url ? hit.parsed_url.search : null,
+    pathname: hit.parsed_url ? hit.parsed_url.pathname : null,
+  };
+  return Object.keys(payload).map(function(key) {return encode(payload[key]);}).join(',');
 }
 
 // encode commas

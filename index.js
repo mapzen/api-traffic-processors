594b74df9a567551e06c84421810dd5ab7ec3465
Evan Griffiths <griffithse@gmail.com>
2016-04-07 11:05:50 -0400

    log entire hit

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ module.exports = function() {
 //   is_keyless
 //   parsed_url
 function formatHit(hit) {
-  var payload = {
-    timestamp: new Date().getTime(),
-    api: hit.api_name,
-    key: hit.key_name,
-    status: hit.status_code,
-    query: hit.parsed_url ? hit.parsed_url.search : null,
-    pathname: hit.parsed_url ? hit.parsed_url.pathname : null,
-  };
-  return Object.keys(payload).map(function(key) {return encode(payload[key]);}).join(',');
+  // var payload = {
+  //   timestamp: new Date().getTime(),
+  //   api: hit.api_name,
+  //   key: hit.key_name,
+  //   status: hit.status_code,
+  //   query: hit.parsed_url ? hit.parsed_url.search : null,
+  //   pathname: hit.parsed_url ? hit.parsed_url.pathname : null,
+  // };
+  return Object.keys(hit).map(function(key) {return encode(payload[key]);}).join(',');
 }
 
 // encode commas

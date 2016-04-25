e1b8b94ece04f2fe1867f7c371dfdbba53cfb2f4
Evan Griffiths <griffithse@gmail.com>
2016-04-25 14:32:56 -0400

    kinesis config update

--- processors/apiaxleKinesis.js
+++ processors/apiaxleKinesis.js
@@ -2,8 +2,8 @@ var KinesisExporter = require('../exporters/kinesisExporter.js');
 var parser = require('../parsers/apiaxleParser.js');
 var formatter = require('../formatters/trafficSpaces.js');
 
-module.exports = function apiaxleKinesis() {
-  var exporter = new KinesisExporter(process.env.FIREHOSE_STREAM);
+module.exports = function apiaxleKinesis(args) {
+  var exporter = new KinesisExporter(args.streamName, args.region);
 
   this.processHit = function processHit(hit, cb) {
     var payload;
--- utility/exportFileToKinesis.js
+++ utility/exportFileToKinesis.js
@@ -1,23 +1,24 @@
 var fs = require('fs');
 
-if (process.argv.length < 4) {
-  console.log('need to call with src filename and parser');
+if (process.argv.length < 6) {
+  console.log('need to call with src, parser, streamname, awsregion');
   return;
 }
 
-function exportFileToKinesis(src, parserpath) {
+function exportFileToKinesis(src, parserpath, streamname, region) {
   var parser = require(parserpath);
   var formatter = require('../formatters/trafficSpaces.js');
   var KinesisExporter = require('../exporters/kinesisExporter.js');
-  var exporter = new KinesisExporter('api_hits_processing_dev', 'us-east-1');
+  var exporter = new KinesisExporter(streamname, region);
   var lines = fs.readFileSync(src).toString().split('\n');
   var i;
   for (i = 0; i < lines.length; i++) {
     if (!lines[i]) continue;
     exporter.add(formatter(parser(lines[i])));
-    // output.push(formatter(parser(lines[i])));
   }
   console.log('done');
 }
 
-exportFileToKinesis(process.argv[2], process.argv[3]);
+exportFileToKinesis(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
+
+var exporter = new KinesisExporter('api_hits_processing_dev', 'us-east-1');

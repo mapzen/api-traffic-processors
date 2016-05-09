3185e0a671c32f1788ed1b385b735824e142b3ba
Evan Griffiths <griffithse@gmail.com>
2016-05-09 15:18:07 -0400

    improve error handling/reporting

--- processors/lambdaS3.js
+++ processors/lambdaS3.js
@@ -13,46 +13,56 @@ module.exports = function (config) {
   var destPrefix = config.destPrefix;
 
   return function handler(event, context, callback) {
+    Error.stackTraceLimit = 3;
+
     var srcBucket = event.Records[0].s3.bucket.name;
     var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     var keyComponents = srcKey.split('/');
     var service = keyComponents[keyComponents.length - 2];
     var filename = keyComponents[keyComponents.length - 1];
     var parser = parsers[service];
-    if (!parser) return callback(new Error('no parser for: ' + service));
+    if (!parser) {
+      return callback(new Error('Error: no parser for ' + service + '\nS3Key: ' + srcKey));
+    }
 
-    console.log('fetching ' + srcKey);
     s3.getObject({ Bucket: srcBucket, Key: srcKey }, function getS3Object(err, data) {
-      if (err) return callback(err);
+      if (err) return callback(new Error('Error: s3 get failed with ' + err.message));
       var lines = data.Body.toString().split('\n');
-      console.log('retrieved ' + lines.length + ' lines');
       var formatted = [];
       lines.forEach(function (line) {
         if (!line) return;
         formatted.push(formatter(parser(line)));
       });
-      exporter.addBatch(formatted);
-
-      // send log files to legacy parsing location
-      // (will be removed once the legacy system is shut down)
-      s3.copyObject({
-        Bucket: destBucket,
-        CopySource: srcBucket + '/' + event.Records[0].s3.object.key,
-        Key: destPrefix + '/' + service + '/' + filename
-      }, function (copyerr) {
-        if (copyerr) {
-          console.error(copyerr);
-          return callback(copyerr);
+      exporter.addBatch(formatted, function (exporterr, exportresult) {
+        if (exporterr) {
+          var newerr = new Error('Error: exporter.addBatch failed for ' + srcKey +
+                                 '\n' + exporterr.message);
+          newerr.stack = exporterr.stack + '\n' + newerr.stack;
+          return callback(newerr);
         }
-        s3.deleteObject({
-          Bucket: srcBucket,
-          Key: event.Records[0].s3.object.key
-        }, function (deleteerr) {
-          if (err) {
-            console.error(deleteerr);
-            return callback(deleteerr);
+
+        // send log files to legacy parsing location
+        // (will be removed once the legacy system is shut down)
+        s3.copyObject({
+          Bucket: destBucket,
+          CopySource: srcBucket + '/' + event.Records[0].s3.object.key,
+          Key: destPrefix + '/' + service + '/' + filename
+        }, function (copyerr) {
+          if (copyerr) {
+            console.error('Error: s3 copy failed for ' + srcKey + '\n' + copyerr.message);
+            return callback(null);
           }
-          callback();
+          s3.deleteObject({
+            Bucket: srcBucket,
+            Key: event.Records[0].s3.object.key
+          }, function (deleteerr) {
+            if (err) {
+              console.error('Error: s3 delete failed for ' + srcKey + '\n' + deleteerr.message);
+            } else {
+              console.log('Loaded ' + srcKey + ' and ' + exportresult);
+            }
+            callback(null);
+          });
         });
       });
     });

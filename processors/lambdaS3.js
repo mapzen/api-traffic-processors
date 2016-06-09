7abf79e2bcdd86c1a797cb40ba4b55bf431e0619
Evan Griffiths <griffithse@gmail.com>
2016-06-09 17:43:22 -0400

    stop moving fastly logs

--- processors/lambdaS3.js
+++ processors/lambdaS3.js
@@ -9,9 +9,6 @@ module.exports = function (config) {
   var formatter = config.formatter;
   var exporter = config.exporter;
 
-  var destBucket = config.destBucket;
-  var destPrefix = config.destPrefix;
-
   return function handler(event, context, callback) {
     Error.stackTraceLimit = 3;
 
@@ -19,7 +16,6 @@ module.exports = function (config) {
     var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     var keyComponents = srcKey.split('/');
     var service = keyComponents[keyComponents.length - 2];
-    var filename = keyComponents[keyComponents.length - 1];
     var parser = parsers[service];
     if (!parser) {
       return callback(new Error('Error: no parser for ' + service + '\nS3Key: ' + srcKey));
@@ -35,37 +31,14 @@ module.exports = function (config) {
         if (!line) return;
         formatted.push(formatter(parser(line)));
       });
-      exporter.addBatch(formatted, function (exporterr, exportresult) {
+      exporter.addBatch(formatted, function (exporterr) {
         if (exporterr) {
           var newerr = new Error('Error: exporter.addBatch failed for ' + srcKey +
                                  '\n' + exporterr.message);
           newerr.stack = exporterr.stack + '\n' + newerr.stack;
           return callback(newerr);
         }
-
-        // send log files to legacy parsing location
-        // (will be removed once the legacy system is shut down)
-        s3.copyObject({
-          Bucket: destBucket,
-          CopySource: srcBucket + '/' + event.Records[0].s3.object.key,
-          Key: destPrefix + '/' + service + '/' + filename
-        }, function (copyerr) {
-          if (copyerr) {
-            console.error('Error: s3 copy failed for ' + srcKey + '\n' + copyerr.message);
-            return callback(null);
-          }
-          s3.deleteObject({
-            Bucket: srcBucket,
-            Key: srcKey
-          }, function (deleteerr) {
-            if (deleteerr) {
-              console.error('Error: s3 delete failed for ' + srcKey + '\n' + deleteerr.message);
-            } else {
-              console.log('Loaded ' + srcKey + ' and ' + exportresult);
-            }
-            callback(null);
-          });
-        });
+        callback(null);
       });
     });
   };

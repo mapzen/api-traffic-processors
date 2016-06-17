7905f36f80f67240da632d9656bb3946c5c9533b
Evan Griffiths <griffithse@gmail.com>
2016-06-17 15:17:42 -0400

    add encoding sortkey and distkey to api_hits

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS api_hits (ts TIMESTAMP, api TEXT, key TEXT, status TEXT, origin TEXT, duplicate BOOLEAN);
+CREATE TABLE IF NOT EXISTS api_hits (ts TIMESTAMP SORTKEY ENCODE delta, api VARCHAR(256) ENCODE lzo, key VARCHAR(256) DISTKEY ENCODE lzo, status VARCHAR(256) ENCODE lzo, origin VARCHAR(256) ENCODE lzo, duplicate BOOLEAN);
 
 CREATE TABLE IF NOT EXISTS pelias_traffic_v2 (date TIMESTAMP, api_key VARCHAR(100), path VARCHAR(250), query VARCHAR(400), status INTEGER, http_method VARCHAR(100), cache_hit BOOLEAN, total_ms INTEGER, first_byte_ms INTEGER);
 

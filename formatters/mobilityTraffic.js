0d00f80727800ef25623c64aa0a65962a4e32aa3
Evan Griffiths <griffithse@gmail.com>
2016-11-07 10:14:45 -0500

    use correct fields for mobility_traffic and decodeuri the search column

--- formatters/mobilityTraffic.js
+++ formatters/mobilityTraffic.js
@@ -6,7 +6,7 @@ module.exports = function vectorTraffic(fields) {
     fsv.escapeField(fields.api, 100),
     fsv.escapeField(fields.key, 100),
     fsv.escapeField(fields.status, 100),
-    fsv.escapeField(fields.path, 250),
-    fsv.escapeField(fields.query, 400)
+    fsv.escapeField(fields.pathname, 250),
+    fsv.escapeField(fields.search, 400)
   ].join(' ');
 };
